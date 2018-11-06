fe78cddfef3b8ce4c1d665fef9a334fae879340f
Cailet Latham <cailet@bink.com>
2018-11-06 14:12:33 +0000

    Added journald logging

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,8 @@
 include_recipe 'datadog::dd-agent'
+
+directory '/etc/datadog-agent/conf.d/journald.d'
+
+file '/etc/datadog-agent/conf.d/journald.d/conf.yaml' do
+  action :create
+  content "logs:\n  - type: journald\n    path: /run/log/journal"
+end
