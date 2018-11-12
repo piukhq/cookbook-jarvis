28e6b583b19d479cf4a419010483f910572f3f87
Cailet Latham <cailet@bink.com>
2018-11-12 17:10:18 +0000

    Fixed journal logging

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name 'jarvis'
 description 'Build and Deploy Datadog Agent'
-version '1.0.1'
+version '1.0.2'
 
 depends 'datadog'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,13 @@
 include_recipe 'datadog::dd-agent'
 
+service 'datadog-agent' do
+  action :nothing
+end
+
 directory '/etc/datadog-agent/conf.d/journald.d'
 
 file '/etc/datadog-agent/conf.d/journald.d/conf.yaml' do
   action :create
-  content "logs:\n  - type: journald\n    path: /run/log/journal"
+  content "logs:\n  - type: journald\n"
+  notifies :restart, 'service[datadog-agent]'
 end
